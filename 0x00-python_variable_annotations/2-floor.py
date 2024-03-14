9d826eca8cacd999a2b5a15387f07d96ed97917e
Lwazi Ncube <lwazincubex@gmail.com>
2024-03-15 00:14:24 +0200

    floor function takes a float n and returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function floor which takes a float n
+as argument and returns the floor of the float."""
+
+
+def floor(n: float) -> float:
+    """floor function takes a float n and returns the floor of the float"""
+    return n
