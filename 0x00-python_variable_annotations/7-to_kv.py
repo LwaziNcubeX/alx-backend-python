52aff10386ecd1a92282dc939f79ac9b457f249d
Lwazi Ncube <lwazincubex@gmail.com>
2024-03-15 01:17:43 +0200

    removed a dict and added a tuple in return statement

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,4 +6,4 @@ import typing
 
 def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
     """takes a string k and an int or float v and returns a tuple"""
-    return {k, v * v}
+    return k, v ** 2
