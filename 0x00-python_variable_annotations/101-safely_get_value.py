c7e45106c8f796c2921a3770e26568e0cccb6523
Lwazi Ncube <lwazincubex@gmail.com>
2024-03-15 02:18:23 +0200

    restore last commit

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A function that returns values safely with type annotations."""
+"""A function that return values, add type annotations to the function """
 import typing
 
 
@@ -7,22 +7,7 @@ def safely_get_value(dct: typing.Mapping, key: typing.Any,
                      default: typing.Union[typing.TypeVar, None] = None
                      ) -> typing.Union[typing.Any, typing.TypeVar]:
     """
-    Safely retrieves a value from a mapping.
-
-    Retrieves a value associated with the given key from the provided mapping.
-    If the key exists in the mapping, the corresponding value is returned. If
-    the key does not exist, the specified default value is returned instead.
-
-    Args:
-        dct (typing.Mapping): The mapping (e.g., dictionary) to retrieve
-            the value from.
-        key (typing.Any): The key whose associated value is to be retrieved.
-        default (typing.Union[typing.TypeVar, None], optional): The value to
-            return if the key is not found in the mapping. Defaults to None.
-
-    Returns:
-        typing.Union[typing.Any, typing.TypeVar]: The value associated with the
-        key if found in the mapping, or the default value otherwise.
+    A function that return values, add type annotations to the function
     """
     if key in dct:
         return dct[key]
