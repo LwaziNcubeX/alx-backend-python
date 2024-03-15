1a33b3fd1593f2cef7f16b6d4051852b661e8cfd
Lwazi Ncube <lwazincubex@gmail.com>
2024-03-15 02:03:32 +0200

    Update docs

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,8 +7,9 @@ import typing
 def safely_get_value(dct: typing.Mapping, key: typing.Any,
                      default: typing.Union[typing.TypeVar, None] = None
                      ) -> typing.Union[typing.Any, typing.TypeVar]:
-    """A function that return values,
-    add type annotations to the function """
+    """
+    A function that return values, add type annotations to the function
+    """
     if key in dct:
         return dct[key]
     else:
