74b98d9ad28945406c492d056792479e4d80dd6f
Lwazi Ncube <lwazincubex@gmail.com>
2024-03-19 00:46:52 +0200

    Add selection sort

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,21 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
+def selection_sort(arr):
+    """
+    Selection sort for an array
+    :param arr:
+    :return:
+    """
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     runs wait_random n times with the specified max_delay.
@@ -20,4 +35,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for i in range(n):
         a = await wait_random(max_delay)
         my_list.append(a)
+
+    selection_sort(my_list)
     return my_list
