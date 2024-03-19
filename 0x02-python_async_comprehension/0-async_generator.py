50b937faff07dcf3a1eb3aee93d32e18c3bdfb51
Lwazi Ncube <lwazincubex@gmail.com>
2024-03-19 17:00:43 +0200

    use old version of async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ A coroutine called async_generator that takes no arguments.
 """
 import asyncio
 from random import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Loops 10 times and sleeps 1 sec per loop then yields them
     """
