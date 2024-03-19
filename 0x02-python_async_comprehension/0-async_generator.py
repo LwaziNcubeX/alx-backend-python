083fcfeaa801c4d513465888ddf308a5a66d4467
Lwazi Ncube <lwazincubex@gmail.com>
2024-03-19 14:59:24 +0200

    Import and return typing AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ A coroutine called async_generator that takes no arguments.
 """
 import asyncio
 from random import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Loops 10 times and sleeps 1 sec per loop then yields them
     """
